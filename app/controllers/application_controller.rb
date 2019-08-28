97c623e6a6799d9270571c86716172db5f3f2a51
Anatasha <anatashadyahia.88@gmail.com>
2019-08-28 18:00:33 +0800

    create questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,23 @@ class ApplicationController < Sinatra::Base
   set :views, File.expand_path('../../views', __FILE__)
 
   get '/' do
+    @questions = Questions.all
     erb :index
   end
+
+  get "/questions/new" do
+    @question = Question.new
+    erb :"question/new"
+  end
+
+  post "/questions" do
+    @question = Question.new(params[:question])
+
+    if question.save
+      redirect to("/questions")
+    else
+      erb :"questions/new"
+  end
+
+
 end
